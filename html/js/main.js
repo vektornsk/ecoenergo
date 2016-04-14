faf1ddaa3f0a824f3da55fca0b7883b33977e7ab
VasiliyP <vektornsk@yandex.ru>
2016-04-14 16:15:28 +0600

    head-footer-ok

--- html/js/main.js
+++ html/js/main.js
@@ -7,18 +7,32 @@ $(function() {
 			$(this).parent().parent().addClass("action")
 		}, function() {
 			$(this).parent().parent().removeClass("action")
-
+			
 		}
 	);
-
+	
 	$(".menu-sub-sub__item").hover(
 		function() {
 			$(this).parent().parent().addClass("action")
 		}, function() {
 			$(this).parent().parent().removeClass("action")
-
+			
 		}
 	);
+	
+//=== Fix меню ===
+	var stickyHeaderTop = $('#nav').offset().top;
 
+	$(window).scroll(function(){
+		if( $(window).scrollTop() > stickyHeaderTop ) {
+			$('#nav').css({position: 'fixed', top: '0px', width: '100%', 'z-index': '10', 'box-shadow': '1px 2px 5px 0.00px rgba(0, 0, 0, 0.37)'});
+			$('#nav').css('display', 'block');
+			$('.menu__item_catalog').removeClass('open');
+		} else {
+			$('#nav').css({position: 'static', top: '0px', 'box-shadow': 'none'});
+			$('.menu__item_catalog').addClass('open');
+			
+		}
+	});
 
 }); //$
