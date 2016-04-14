a34d0ea2623e73800d27b0285ebc6d7615ad9c9c
VasiliyP <vektornsk@yandex.ru>
2016-04-14 14:32:27 +0600

    menu

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
+//=== Меню ховер ===
+	
+	$(".menu-sub__item").hover(
+		function() {
+			$(this).parent().parent().addClass("action")
+		}, function() {
+			$(this).parent().parent().removeClass("action")
+
+		}
+	);
+
+	$(".menu-sub-sub__item").hover(
+		function() {
+			$(this).parent().parent().addClass("action")
+		}, function() {
+			$(this).parent().parent().removeClass("action")
+
+		}
+	);
 
 
-});
+}); //$
